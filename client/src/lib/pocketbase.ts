00c5d030e52a29dec8fa52bd50f1b2ed0077e811
jinjintv <theffffff@naver.com>
2026-01-01 06:53:49 +0900

    ddddddssssss

--- client/src/lib/pocketbase.ts
+++ client/src/lib/pocketbase.ts
@@ -1,6 +1,6 @@
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase('https://remarkable-enthusiasm.railway.app');
+const pb = new PocketBase('https://next-challenge-web-production.up.railway.app');
 
 // React strict mode + route 이동 시 요청 취소 방지
 pb.autoCancellation(false);
