5559a0e4df7cffdac0f3057105cc15b619227ff1
jinjintv <theffffff@naver.com>
2026-01-01 03:43:17 +0900

    ddddd

---
+++ client/vite.config.ts
@@ -0,0 +1,15 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // @ 기호를 src 폴더 경로로 매핑합니다.
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  // ... 기타 설정
+});
